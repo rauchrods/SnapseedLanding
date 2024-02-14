1f74257290ba2137795f946df83ee96744fb6251
Rauch Rodrigues <rauch.rodrigues@koseapp.com>
2024-02-14 17:54:26 +0530

    optimised description block, added scroll animation to header image

---
+++ src/data/descriptionBlockData.js
@@ -0,0 +1,49 @@
+import descImg1 from "../assets/desc_1.png";
+import descImg2 from "../assets/desc_2.png";
+import descImg3 from "../assets/desc_3.png";
+import descImg4 from "../assets/desc_4.png";
+import descImg5 from "../assets/desc_5.png";
+import descImg6 from "../assets/desc_6.png";
+import descImg7 from "../assets/desc_7.png";
+
+export const descriptions = [
+  {
+    img: descImg7,
+    description:
+      "Achieve success, one step at a time with Stepseed: Your AI Accountability Partner",
+  },
+  {
+    img: descImg1,
+    description:
+      "Automatically divide your Goals into attainable milestones using AI",
+
+    alignment: "right",
+  },
+  {
+    img: descImg2,
+    description: "Analytics to track your progress and gain insights",
+  },
+  {
+    img: descImg3,
+    description:
+      "Meet Seedy, your personal AI life goal coach. Seedy can help you create action plans, provide advice and motivation",
+    alignment: "right",
+  },
+  {
+    img: descImg4,
+    description:
+      "Add unlimited journal entries and see how far you've come on your journey towards achieving your goals.",
+  },
+  {
+    img: descImg5,
+    description:
+      "if you ever run out of motivation, check the quote of the day!",
+
+    alignment: "right",
+  },
+  {
+    img: descImg6,
+    description:
+      "Accesscurated video content that will help you stay on the right track",
+  },
+];
